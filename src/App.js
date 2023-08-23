a42d25ff207e9be2dd2c6eb937476cad898216c1
Ali <Akstudid@mail.com>
2023-08-23 16:11:14 +0330

    TaskItem Module Is Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import logo from './logo.svg';
 import './App.css';
-
+import $ from 'jquery';
+import Popper from 'popper.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Topitem from './Topitem';
+import Taskitems from './Taskitems';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container w-100 h-100 p-4'>
+      <div className='row h-100 justify-content-center align-align-items-start'>
+        <div className='col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h-fit'>
+          <Topitem />
+          <Taskitems />
+        </div>
+      </div>
     </div>
   );
 }
